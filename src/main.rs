a46d4df83a3d36eb6116cbc7e23901e6a9575789
Tomme <tomme87@gmail.com>
2023-03-03 22:15:25 +0100

    openweathermap package is broken. manually do the request instead

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::env;
 use futures::prelude::*;
 use irc::client::prelude::*;
-use openweathermap::weather;
-use regex::Regex;
+use regex::{Regex};
 use rusqlite::Connection;
+use reqwest::Url;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct WeatherData {
+    name: String,
+    description: String,
+    temperature: f64,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), failure::Error> {
@@ -41,19 +49,18 @@ async fn main() -> Result<(), failure::Error> {
         if let Command::PRIVMSG(channel, msg) = message.command {
             if let Some(caps) = re.captures(msg.as_str()) {
                 let location = caps.get(1).unwrap().as_str();
-                if send_weather(location, &channel, &client, &api_key).await {
-                    connection.execute("INSERT INTO user (name, last_location) VALUES(?1,?2) ON CONFLICT(name) DO UPDATE SET last_location=?3", [
-                        nick.as_str(),
-                        location,
-                        location
-                    ]).unwrap();
-                }
+                send_weather(location, &channel, &client, &api_key).await.unwrap();
+                connection.execute("INSERT INTO user (name, last_location) VALUES(?1,?2) ON CONFLICT(name) DO UPDATE SET last_location=?3", [
+                    nick.as_str(),
+                    location,
+                    location
+                ]).unwrap();
             } else if msg.eq("!v") {
                 let mut statement = connection.prepare("SELECT last_location FROM user WHERE name = ?").unwrap();
                 let mut rows = statement.query([nick.as_str()]).unwrap();
                 if let Some(row) = rows.next()? {
                     let location = row.get::<usize, String>(0).unwrap();
-                    send_weather(location.as_str(), &channel, &client, &api_key).await;
+                    send_weather(location.as_str(), &channel, &client, &api_key).await.unwrap();
                 }
             }
         }
@@ -62,15 +69,47 @@ async fn main() -> Result<(), failure::Error> {
     Ok(())
 }
 
-async fn send_weather(location: &str, channel: &str, client: &Client, api_key: &str) -> bool {
-    match &weather(location, "metric", "no", api_key).await {
-        Ok(current) => {
-            client.send_privmsg(&channel, format!("{} nå: {}°C, {}", current.name.as_str(), current.main.temp, current.weather[0].description.as_str())).unwrap();
-            true
-        },
-        Err(e) => {
-            client.send_privmsg(&channel, e).unwrap();
-            false
-        }
+async fn send_weather(location: &str, channel: &str, client: &Client, api_key: &str) -> Result<(), failure::Error> {
+    let message = match get_weather(location, api_key).await {
+        Ok(weather_data) => format!("{} nå: {}°C, {}", weather_data.name.as_str(), weather_data.temperature, weather_data.description.as_str()),
+        Err(err) => err.to_string()
+    };
+
+    client.send_privmsg(&channel, message).unwrap();
+    Ok(())
+}
+
+async fn get_weather(city: &str, api_key: &str) -> Result<WeatherData, Box<dyn std::error::Error>> {
+    let url = Url::parse_with_params(
+        "http://api.openweathermap.org/data/2.5/weather",
+        &[
+            ("q", city),
+            ("appid", api_key),
+            ("units", "metric"),
+            ("lang", "no"),
+        ],
+    )?;
+
+    let response = reqwest::get(url).await?;
+
+    if !response.status().is_success() {
+        let response_text = response.text().await?;
+        return Err(response_text.into())
     }
+
+    let response_text = response.text().await?;
+
+    let data: serde_json::Value = serde_json::from_str(&response_text)?;
+
+    let name = data["name"].as_str().unwrap().to_string();
+    let description = data["weather"][0]["description"].as_str().unwrap().to_string();
+    let temperature = data["main"]["temp"].as_f64().unwrap();
+
+    let weather_data = WeatherData {
+        name,
+        description,
+        temperature,
+    };
+
+    Ok(weather_data)
 }
